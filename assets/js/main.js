f18dd649ae3d9ceaf844963347730e39730c3a06
cipher <cipherlofi04@gmail.com>
2023-05-14 02:37:38 +0300

    making nav bar blurry when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,10 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
